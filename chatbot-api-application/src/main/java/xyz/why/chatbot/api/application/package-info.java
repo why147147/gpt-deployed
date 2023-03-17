396a62fcdbfa42488ddcb10160b3611a26222dbf
WHY <whyggwp@163.com>
2023-03-17 23:37:53 +0800

    WHY: feature: init ddd project

---
+++ chatbot-api-application/src/main/java/xyz/why/chatbot/api/application/package-info.java
@@ -0,0 +1,1 @@
+package xyz.why.chatbot.api.application;
---
+++ chatbot-api-interfaces/src/main/java/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Title: ApiApplication
+ * @Author WHY
+ * @Date: 2023/3/17 23:13
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
